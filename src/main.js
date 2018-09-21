9b5e85a158efc6bbe57a35b969856204fb51d283
Robert Holwerda <robert.holwerda@han.nl>
2018-09-21 19:53:05 +0200

    project setup

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'main.sass';
+
+console.log("HI THERE!!");
+
+ReactDOM.render(<App/>, document.getElementById("react-root"))
+
